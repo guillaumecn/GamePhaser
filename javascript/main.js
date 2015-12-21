1e462e2368e0dd40084d54f5c813fddfe8b08be5
guillaumecn <chabot.nobert.guillaume@gmail.com>
2015-12-21 17:06:06 -0500

    ajout fonction de creation de map. Contour de la map avec entrée et sortie aléatoire

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,39 @@
-var game = new Phaser.Game(800,600, Phaser.AUTO, 'gamebox', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800,600, Phaser.AUTO, 'gamebox', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
     game.load.image('sky', '../assets/sky.png');
     game.load.image('ground', '../assets/platform.png');
     game.load.image('star', '../assets/star.png');
+    game.load.image('ground_1x1', '../assets/ground_1x1.png');
     game.load.spritesheet('dude1', '../assets/dude1.png', 32, 32);
 }
 
-
+//Constantes*****
+var LARGEURJEUX = 25; //nombre de tiles de large du jeux
+var HAUTEURJEUX = 19; //nombre de tiles de haut du jeux
+var VITESSEJOUEUR = 300; //vitesse de déplacement du joueur
 
 //var players;
-var terrain;
-var cursors;
+var terrain; //Groupe contenant les objet innanimé
+var cursors; //Variable du curseur (flèche)
+var currentLayer;  //Variable qui dit on est présentement sur quel layer
+var map;
+var layer1;
+var entree_x;
+var sortie_x;
 
 function create() {
-
-//Background sky
-//game.add.sprite(0,0,'sky');
 
 
 
 //Add physic to the game
 game.physics.startSystem(Phaser.Physics.ARCADE);
-game.stage.backgroundColor = '#2d2d2d';
+
+//**************************Map*************************//
+createmap();
+
+//******************************************************//
 
 //On crée le groupe d'objet inerte
 terrain = game.add.group();
@@ -34,8 +44,7 @@ terrain.enableBody= true;
 players = game.add.group();
 
 //On crée le joueur 1
-player1 = players.create(50,50,'dude1');
-//player1 = game.add.sprite(50,50,'dude');
+player1 = players.create(entree_x*32,0,'dude1');
 //On fait suivre la camera sur le joueur
 game.camera.follow(player1);
 //On active et set la physique du joueur 1
@@ -50,7 +59,6 @@ player1.animations.add('right', [6,7,8,7], 10, true);
 //On crée le curseur
 cursors = game.input.keyboard.createCursorKeys();
 
-
 }
 
 function update() {
@@ -59,6 +67,9 @@ function update() {
 var animation = 'none';
 
 //On fait l'animation du joueur*************************
+
+    //collision entre le joueur et les obstacle de la layer 1
+    game.physics.arcade.collide(player1, layer1);
     //  Reset the players velocity (movement)
     player1.body.velocity.x = 0;
     player1.body.velocity.y = 0;
@@ -66,25 +77,25 @@ var animation = 'none';
     if (cursors.left.isDown)
     {
         //Move to the left
-        player1.body.velocity.x = -150;
+        player1.body.velocity.x = -VITESSEJOUEUR;
         animation = 'left';
     }
     if (cursors.right.isDown)
     {
         //Move to the right
-        player1.body.velocity.x = 150;
+        player1.body.velocity.x = VITESSEJOUEUR;
         animation = 'right';
     }
     if (cursors.up.isDown)
     {
         //Move up
-        player1.body.velocity.y = -150;
+        player1.body.velocity.y = -VITESSEJOUEUR;
         animation = 'up';
     }
     if (cursors.down.isDown)
     {
         //Move down
-        player1.body.velocity.y = 150;
+        player1.body.velocity.y = VITESSEJOUEUR;
         animation = 'down';
     }
 
@@ -100,3 +111,72 @@ var animation = 'none';
 }
 
 
+function render() {
+
+   //game.debug.text('Current Layer: ' + currentLayer.name, 50, 500);
+   //game.debug.text('Position en x  ' + tile_x, 50, 500);
+   //game.debug.text('Position en y ' + tile_y, 50, 550);
+    
+
+}
+
+function createmap() {
+
+
+    //simple background to the map
+    game.stage.backgroundColor = '#2d2d2d';
+
+    //  Creates a blank tilemap
+    map = game.add.tilemap();
+
+    //  Add a Tileset image to the map
+    map.addTilesetImage('ground_1x1');
+
+    //  Creates a new blank layer and sets the map dimensions.
+    //  In this case the map is LARGEURJEUX x HAUTEURJEUX tiles in size and the tiles are 32x32 pixels in size.
+    layer1 = map.create('level 1',LARGEURJEUX , HAUTEURJEUX, 32, 32);
+    layer1.scrollFactorX = 0.5;
+    layer1.scrollFactorY = 0.5;
+    currentLayer = layer1;
+    
+    //On set la grosseur du monde selon la layer1
+    layer1.resizeWorld();
+    //On fait ensorte que les tiles 0 à 24 du tilesetimage soit "dur" (collision) dans la layer 1.
+    map.setCollisionBetween(0, 24, true, layer1); 
+
+    //*******On fait le contour de la map avec une entrée et une sortie************
+
+    //On trouve la position d'entrée et de sortie du maze
+    entree_x = game.rnd.between(1,LARGEURJEUX-2);
+    sortie_x = game.rnd.between(1,LARGEURJEUX-2);
+    //On met le mur du top et bas
+    for (var i=0;i<LARGEURJEUX;i++)
+    {
+        //Top
+        if (i != entree_x)
+        {
+            map.putTile(2,i,0,layer1);
+        }
+        //Bas
+        if (i != sortie_x)
+        {
+            map.putTile(2,i,HAUTEURJEUX-1);
+        }
+    }
+    for (i=0;i<HAUTEURJEUX;i++)
+    {
+        //droite
+        map.putTile(2,0,i,layer1);
+        //Gauche
+        map.putTile(2,LARGEURJEUX-1,i,layer1);
+    }
+
+    //**************************************************************************
+    //On trouve une position aléatoire 
+    var tile_x = game.rnd.between(1,LARGEURJEUX-1);
+    var tile_y = game.rnd.between(1,HAUTEURJEUX-1);    
+    //On met une tile (la tile 3 du set) à la case trouver(en x et y) sur la map, sur la layer 1.
+    //map.putTile(2,tile_x,tile_y, layer1);
+
+
+}
